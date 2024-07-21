b1c4d048ac57a7bf13abc062a06a3c849cb22ec8
shivammishra6 <shivammmishra6203@gmail.com>
2024-07-21 22:25:57 +0530

    made service with java

---
+++ app/src/main/java/com/example/service/MyService.java
@@ -0,0 +1,33 @@
+package com.example.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.provider.Settings;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    private MediaPlayer player;
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        player=MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
+        player.setLooping(true);
+        player.start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        player.stop();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
